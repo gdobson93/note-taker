53d519a09fcfea626a80d023301bf5f421d4467f
Garrett Dobson <garrettdobson33@gmail.com>
2020-09-08 22:01:06 -0400

    setup server page

---
+++ server.js
@@ -0,0 +1,16 @@
+// use class examples on setting up server
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//middleware that accesses the public folder 
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
